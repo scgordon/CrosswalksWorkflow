7fcdfcc923b68c5ba638bf89ceb6fd08978fee36
scgordon <scgordon@hdfgroup.org>
2016-04-05 15:36:25 -0600

    initialIngest
    brought dryad collections into workflow, created

--- scripts/writeAllRubricTransforms.sh
+++ scripts/writeAllRubricTransforms.sh
@@ -2,8 +2,10 @@
 # It requires two arguments.
 # Argument 1 is the tag element's content for the recommendation components desired (cw:spiral/cw:tag).
 # Argument 2 is the cw:dialect/cw:code of the records you wish to assess
-./rubricTransform.sh FGDC EML
-./rubricTransform.sh FGDC CSDGM
-./rubricTransform.sh FGDC BDP
+#./rubricTransform.sh FGDC EML
+#./rubricTransform.sh FGDC CSDGM
+#./rubricTransform.sh FGDC BDP
 ./rubricTransform.sh FGDC Onedcx
 ./rubricTransform.sh FGDC Dryad
+./rubricTransform.sh DCITE Onedcx
+./rubricTransform.sh DCITE Dryad
